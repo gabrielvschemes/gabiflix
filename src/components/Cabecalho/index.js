8433535ceb15d0023988d5973c45f9e8d63a136f
gabrielvschemes <86749637+gabrielvschemes@users.noreply.github.com>
2021-08-16 19:04:33 -0300

    Trabalho de conclusão de curso

---
+++ src/components/Cabecalho/index.js
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+import './index.css';
+import Logo from './marca.png';
+import Barra from './Lupa.png';
+
+export default ({black}) => {
+    return(
+        <header className={black ? 'black' : ''}>
+            <div className="header--logo">
+                <a href="/">
+                <img src={Logo}></img> 
+                </a>
+            </div>
+            <div className="header--user">
+                <a href="/">
+                    <img src="https://mir-s3-cdn-cf.behance.net/project_modules/disp/366be133850498.56ba69ac36858.png" alt="Usuário" />
+                </a>
+                <div className="search-box">
+                    <input type="text" className="search-txt" placeholder="Pesquisar"></input>
+                    <a href="#" class="search-btn">
+                        <img src={Barra} alt="Lupa" height="20" width="20"></img>
+                    </a>
+                </div>
+            </div>
+            
+               </header>
+    )
+}
